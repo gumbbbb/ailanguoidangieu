2e7df28154cf0d617f2a1bf9b2b66d0bf3e4fe9f
gumbbbb <87250855+gumbbbb@users.noreply.github.com>
2021-07-11 18:16:37 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'Bạn uii!',
+    introDesc: `Trái đất có 7 tỉ ngừi
+    Đố bạn biết ai dẻ thưn nhấc`,
+    btnIntro: 'Biếc nè',
+    title: 'Dẻ hưn nhấc thế giớiii',
+    desc: 'Đố biếc luông :3333 ',
+    btnYes: 'Ai zayy :333',
+    btnNo: 'Hông qan tâm :0',
+    question:'Chính là bạn đó còn ai dào đây nựa <33333333',
+    btnReply: 'Ahihiihii',
+    reply: 'Tại dì cậu đáng eo chớ shao <33333333',
+    mess: 'Tui biết mà 🥰. Hong phải cẻm ơn đôuuu 😘😘',
+    messDesc: 'Nói chiện dới ngừi dẻ hưn thứ hai nào :3.',
+    btnAccept: 'Okiiiii ',
+    messLink: 'https://m.me/as.cacf' //m.melink mess của các bạn. VD: https://m.me/nam.nodemy
+}
