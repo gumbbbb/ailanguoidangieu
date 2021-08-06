7072235fb0d4628a4f1778c1241cf970f9cd5e93
gumbbbb <87250855+gumbbbb@users.noreply.github.com>
2021-08-06 22:44:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
-    introTitle: 'Bạn uii!',
+    introTitle: 'Thạo uii!',
     introDesc: `Trái đất có 7 tỉ ngừi
-    Đố bạn biết ai dẻ thưn nhấc`,
+    Đố zk biết ai dẻ thưn nhấc`,
     btnIntro: 'Biếc nè',
     title: 'Dẻ hưn nhấc thế giớiii',
     desc: 'Đố biếc luông :3333 ',
     btnYes: 'Ai zayy :333',
     btnNo: 'Hông qan tâm :0',
-    question:'Chính là bạn đó còn ai dào đây nựa <33333333',
+    question:'Chính là thafuthafu chứ còn ai dào đây nựa <33333333',
     btnReply: 'Ahihiihii',
-    reply: 'Tại dì cậu đáng eo chớ shao <33333333',
-    mess: 'Tui biết mà 🥰. Hong phải cẻm ơn đôuuu 😘😘',
-    messDesc: 'Nói chiện dới ngừi dẻ hưn thứ hai nào :3.',
+    reply: 'Tại dì zk đáng eo chớ shao <33333333',
+    mess: 'Tui xin lũi chiện hồi tối he, hôm lào bùuuu 😘😘',
+    messDesc: 'Chấp nhận lời xin nhỗi nào :3.',
     btnAccept: 'Okiiiii ',
-    messLink: 'https://m.me/as.cacf' //m.melink mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://m.me/annh.tuan.77715' //m.melink mess của các bạn. VD: https://m.me/nam.nodemy
 }
